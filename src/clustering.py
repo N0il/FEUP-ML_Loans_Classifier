450cd2d420fb30f3e7440f8695c566a8bd3446cc
frenato00 <up201908044@up.pt>
2022-12-09 11:56:59 +0000

    clustering changes

--- src/clustering.py
+++ src/clustering.py
@@ -1,3 +1,6 @@
+"""This module is used to execute the clustering data mining task"""
+
+
 import matplotlib.pyplot as plt
 from pipeline import processFeatures
 import seaborn as sns; sns.set()
@@ -6,6 +9,7 @@ from sklearn.cluster import KMeans
 import pandas as pd
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors 
 scaler = StandardScaler()
 
 OUTPUT_DATA_PATH = './../data/output/'
@@ -18,16 +22,16 @@ loansDataFrame = pd.DataFrame(loansDataFrame, columns = ['amount', 'effortRate']
 
 print(loansDataFrame.head())
 
+# ploting the elbow curve for k-means to find the point of diminishing returns of the k variable
 inertia = {}
-
 for k in range(1, 10):
     kmeans = KMeans(n_clusters=k ,init="k-means++")
     kmeans = kmeans.fit(loansDataFrame[['amount', 'effortRate']])
     inertia[k] = kmeans.inertia_
-
 plt.plot(inertia.keys(), inertia.values(), 'gs-')
 plt.show()
 
+# best k-means plot
 kmeans = KMeans(n_clusters=4 ,init="k-means++")
 kmeans = kmeans.fit(loansDataFrame[['amount', 'effortRate']])
 
@@ -44,10 +48,34 @@ loansDataFrame2 = processFeatures(loansDataFrame2, False, 'none', 'none')
 loansDataFrame2 = scaler.fit_transform(loansDataFrame2[['amount', 'effortRate']])
 loansDataFrame2 = pd.DataFrame(loansDataFrame2, columns = ['amount', 'effortRate'])
 
-dbscan = DBSCAN(eps = 0.1, min_samples = 5).fit(loansDataFrame2[['amount', 'effortRate']]) # fitting the model
+
+# Generate elbow plot of distance to neighbours to find the point of diminishing returns for the dbscan max distance
+neighbors = NearestNeighbors(n_neighbors=20)
+neighbors_fit = neighbors.fit(loansDataFrame2)
+distances, indices = neighbors_fit.kneighbors(loansDataFrame2)
+
+distances = np.sort(distances, axis=0)
+distances = distances[:,1]
+plt.plot(distances)
+plt.show()
+    
+
+# We looped through some values of min_samples and eps close to the found point of diminishing returns to find the best clusterings 
+"""
+for m in range(6,8):
+    print(m)
+    eps_range=[x/100.0 for x in range(20,23)]
+    for e in eps_range:
+        dbscan = DBSCAN(eps = e, min_samples = m).fit(loansDataFrame2[['amount', 'effortRate']]) # fitting the model
+        loansDataFrame2['Clusters'] = dbscan.labels_
+        plt.title(f"min_samples={m} and eps={e}")
+        sns.scatterplot(x="amount", y="effortRate",hue = 'Clusters',  data=loansDataFrame2)
+        plt.show()
+"""
+
+# Here is one of the dbscan clusterings with the best separation
+dbscan = DBSCAN(eps = 0.21, min_samples = 6).fit(loansDataFrame2[['amount', 'effortRate']]) # fitting the model
 loansDataFrame2['Clusters'] = dbscan.labels_
 
 sns.scatterplot(x="amount", y="effortRate",hue = 'Clusters',  data=loansDataFrame2)
 plt.show()
-# plt.scatter(loansDataFrame2['amount'], loansDataFrame2['effortRate'], c=labels, cmap= "plasma")
-# plt.show()
